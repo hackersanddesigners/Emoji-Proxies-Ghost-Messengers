efd9d2e5b070c6b0ef9c01fb0fdd388ff7f640ad
Heerko <heerko@swummoq.net>
2022-11-02 16:19:50 +0100

    neopixel example

--- Code/MQTT_neopixel/MQTT_neopixel.ino
+++ Code/MQTT_neopixel/MQTT_neopixel.ino
@@ -1,132 +1,179 @@
-#include <Adafruit_NeoPixel.h>
 #include <WiFi.h>
 #include <MQTT.h>
 #include <ArduinoJson.h>
 // Install this library by going to menu Sketch > Include Libary > Manage Libraries...
 // Search for StringSplitter and click Install.
 #include "StringSplitter.h"
+// Install this library by going to menu Sketch > Include Libary > Manage Libraries...
+// Search for Adafruit Neopixel and click Install.
+#include <Adafruit_NeoPixel.h>
 
-char ssid[] = "wifi network name";  // Replace with your network SSID
-char pass[] = "wifi password";  // Replace with your network password
+char ssid[] = "wifi network name";	// Replace with your network SSID
+char pass[] = "wifi password";			// Replace with your network password
 char host[] = "test.mosquitto.org"; // don't change this.
-char topic[] = "inc-hmm"; // don't change this.
-String client_id = "esp1"; // but please DO change this :)
+char topic[] = "inc-hmm";						// don't change this.
+String client_id = "esp1";					// but please DO change this :)
 
 WiFiClient wifiClient;
 MQTTClient client;
 
 #define LED_PIN 26
-#define NUMPIXELS 8
+#define NUMPIXELS 8 // change this to the number of pixels on the strip
 
 Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
+void setup()
+{
+	Serial.begin(115200);			// serial communication for debugging
+	pinMode(LED_PIN, OUTPUT); // configure pin 26 as a output.
 
-  // start wifi and mqtt
-  WiFi.begin(ssid, pass);
-  client.begin(host, wifiClient);
-  client.onMessage(messageReceived);
-
-  connectWifi();
-  connectMqtt();
-
-  sendMessage("connect");
-
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+	// start wifi and mqtt
+	WiFi.begin(ssid, pass);
+	client.begin(host, wifiClient);
+	client.onMessage(messageReceived); // call the messageReceived function when a message is received
+																		 // connect wifi and mqtt
+	connectWifi();
+	connectMqtt();
+	// say hi
+	sendMessage("connect");
+	// initialize NeoPixel strip object
+	pixels.begin();
 }
 
-void loop() {
-  client.loop();
-  // check if connected
-  if (!client.connected()) {
-    Serial.println("lost connection");
-    connectMqtt();
-  }
+/*
+ * The loop function continuously check for new MQTT messages
+ */
+void loop()
+{
+	client.loop();
+	// check if connected to the MQTT relay server
+	if (!client.connected())
+	{
+		Serial.println("lost connection");
+		connectMqtt();
+		delay(5000); // prevent flooding the server
+	}
 }
 
-void connectWifi() {
-  Serial.print("Connecting to wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("\nWifi connected!");
-  delay(1000);
+/*	
+* The messageReceived function is called when we receive a message over MQTT.
+* Each message is formatted like this: 
+* `origin:command:parameter`
+* `origin` is the sender, here we're looking for messages received from `server`.
+* `command` is the command to be executed, in this example it's the command `led` we're waiting for.
+* `parameter` can contain extra information, here it can be `on` to turn the led on and anything else in the 
+* parameter will turn the led off.
+* 
+* Here we're looking for a message in the format `server:rgb:#nnnnnn`, 
+* where the #nnnnnn part is a hexidecimal color code (see the readme)
+* If we recieve this message and it is formatted properly, color all 
+* the pixels of the ledstrip to this color.
+*/
+
+void messageReceived(String &topic, String &payload)
+{
+	Serial.println(topic + ": " + payload);
+
+	StringSplitter *splitter = new StringSplitter(payload, ':', 3);
+	int itemCount = splitter->getItemCount();
+
+	if (itemCount == 3)
+	{ // we have properly formatted command
+		String origin = splitter->getItemAtIndex(0);
+		String command = splitter->getItemAtIndex(1);
+		String parameter = splitter->getItemAtIndex(2);
+
+		if (origin == "server")
+		{
+			if (command == "rgb")
+			{
+				Serial.println(parameter.length());
+				if (parameter.length() == 7 && parameter.startsWith("#"))
+				{
+					// get the R, G and B parts of the string
+					String r = parameter.substring(1, 3);
+					String g = parameter.substring(3, 5);
+					String b = parameter.substring(5, 7);
+					Serial.print(r);
+					Serial.print(" ");
+					Serial.print(g);
+					Serial.print(" ");
+					Serial.println(b);
+
+					// convert the hex values to decimal
+					int rr = StrToHex(r.c_str());
+					int gg = StrToHex(g.c_str());
+					int bb = StrToHex(b.c_str());
+
+					// then update the pixel colors
+					for (int i = 0; i < NUMPIXELS; i++) { 
+						// pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+						pixels.setPixelColor(i, pixels.Color(rr, gg, bb));
+						pixels.show(); // Send the updated pixel colors to the hardware.
+						delay(100);
+					}
+				}
+			}
+		}
+	}
 }
 
-void connectMqtt() {
-  Serial.print("\nConnecting to MQTT server...");
-  while (!client.connect(client_id.c_str())) {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("\nMQTT connected!");
-  delay(1000);
-  client.subscribe(topic);
+/*
+* This function converts a hexademical number (base 16) into
+* a decimal number. 
+*/
+int StrToHex(const char str[])
+{
+	return (int)strtol(str, 0, 16);
 }
 
-void messageReceived(String &topic, String &payload) {
-  Serial.println(topic + ": " + payload);
+/* ---------------------- you can ignore code below this line --------------------- */
 
-  StringSplitter *splitter = new StringSplitter(payload, ':', 3);
-  int itemCount = splitter->getItemCount();
-
-  if ( itemCount == 3 ) { // we have properly formatted command
-    String origin = splitter->getItemAtIndex(0);
-    String command = splitter->getItemAtIndex(1);
-    String parameter = splitter->getItemAtIndex(2);
-
-    if ( origin == "server" ) {
-      if ( command == "led" ) {
-        if ( parameter == "on" ) {
-          digitalWrite( LED_PIN, HIGH );
-        } else {
-          digitalWrite( LED_PIN, LOW );
-        }
-      }
-      if ( command == "rgb" ) {
-        Serial.println(parameter.length());
-        if ( parameter.length() == 7 && parameter.startsWith("#") ) {
-          String r = parameter.substring(1, 3);
-          String g = parameter.substring(3, 5);
-          String b = parameter.substring(5, 7);
-          Serial.print(r);
-          Serial.print( " ");
-          Serial.print(g);
-          Serial.print( " ");
-          Serial.println(b);
-
-          int rr = StrToHex(r.c_str());
-          int gg = StrToHex(g.c_str());
-          int bb = StrToHex(b.c_str());
-
-          for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-
-            // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-            // Here we're using a moderately bright green color:
-            pixels.setPixelColor(i, pixels.Color(rr, gg, bb));
-
-            pixels.show();   // Send the updated pixel colors to the hardware.
-
-            // delay(DELAYVAL); // Pause before next pass through loop
-          }
-        }
-      }
-    }
-  }
+/*
+ * Wait for wifi connection
+ */
+void connectWifi()
+{
+	Serial.print("Connecting to wifi...");
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print(".");
+		delay(1000);
+	}
+	Serial.println("\nWifi connected!");
+	delay(1000);
 }
 
-int StrToHex(const char str[]) {
-  return (int) strtol(str, 0, 16);
+/*
+ * Wait for MQTT connection and subscribe to the topic
+ */
+void connectMqtt()
+{
+	Serial.print("\nConnecting to MQTT server...");
+	while (!client.connect(client_id.c_str()))
+	{
+		Serial.print(".");
+		delay(1000);
+	}
+	Serial.println("\nMQTT connected!");
+	delay(1000);
+	client.subscribe(topic);
 }
 
-void sendMessage(String message) {
-  sendMessage(message, "");
+/*
+ * Concat the command and parameter into a colon separated string
+ * and publish the message
+ */
+void sendMessage(String message, String parameter)
+{
+	String msg = client_id + ":" + message + ":" + parameter;
+	client.publish(topic, msg);
 }
 
-void sendMessage(String message, String parameter) {
-  String msg = client_id + ":" + message + ":" + parameter;
-  client.publish(topic, msg);
+/*
+ * Overloaded sendMessage function without the parameter
+ */
+void sendMessage(String message)
+{
+	sendMessage(message, "");
 }
--- Code/MQTT_neopixel/readme.md
+++ Code/MQTT_neopixel/readme.md
@@ -1,5 +1,206 @@
 # MQTT Neopixels
 
+In this a somewhat more advanced variation on the [led example](../MQTT_led/).
+Here we connect a piece of individual addressable RGB led strip (called WS2812 or Neopixels). 
+We connect to the MQTT server (topic hmm-inc) and wait for the command:
+`server:rgb:#nnnnnn`. The #nnnnnn is a hexadecimal color code, like the color code used in HTML/CSS[^1]
+There are comment in the code, also included below, that describe how this works. 
+
+Wire GND like so:
+
+| Led Strip  | ESP32       |
+| -----------| ------------|
+| GND  	     | GND         |
+| VCC        | VIN         |
+| DIN        | Pin D26     |
+
+In the images below you see how this should look on a breadboard and what it looks like as a circuit diagram. 
+
+[^1] https://www.freecodecamp.org/news/how-hex-code-colors-work-how-to-choose-colors-without-a-color-picker/
+
+
 ![wiring](MQTT_neopixel_bb.png)
 ![wiring](MQTT_neopixel_sch.png)
 
+```c
+#include <WiFi.h>
+#include <MQTT.h>
+#include <ArduinoJson.h>
+// Install this library by going to menu Sketch > Include Libary > Manage Libraries...
+// Search for StringSplitter and click Install.
+#include "StringSplitter.h"
+// Install this library by going to menu Sketch > Include Libary > Manage Libraries...
+// Search for Adafruit Neopixel and click Install.
+#include <Adafruit_NeoPixel.h>
+
+char ssid[] = "wifi network name";	// Replace with your network SSID
+char pass[] = "wifi password";			// Replace with your network password
+char host[] = "test.mosquitto.org"; // don't change this.
+char topic[] = "inc-hmm";						// don't change this.
+String client_id = "esp1";					// but please DO change this :)
+
+WiFiClient wifiClient;
+MQTTClient client;
+
+#define LED_PIN 26
+#define NUMPIXELS 8 // change this to the number of pixels on the strip
+
+Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+	Serial.begin(115200);			// serial communication for debugging
+	pinMode(LED_PIN, OUTPUT); // configure pin 26 as a output.
+
+	// start wifi and mqtt
+	WiFi.begin(ssid, pass);
+	client.begin(host, wifiClient);
+	client.onMessage(messageReceived); // call the messageReceived function when a message is received
+																		 // connect wifi and mqtt
+	connectWifi();
+	connectMqtt();
+	// say hi
+	sendMessage("connect");
+	// initialize NeoPixel strip object
+	pixels.begin();
+}
+
+/*
+ * The loop function continuously check for new MQTT messages
+ */
+void loop()
+{
+	client.loop();
+	// check if connected to the MQTT relay server
+	if (!client.connected())
+	{
+		Serial.println("lost connection");
+		connectMqtt();
+		delay(5000); // prevent flooding the server
+	}
+}
+
+/*	
+* The messageReceived function is called when we receive a message over MQTT.
+* Each message is formatted like this: 
+* `origin:command:parameter`
+* `origin` is the sender, here we're looking for messages received from `server`.
+* `command` is the command to be executed, in this example it's the command `led` we're waiting for.
+* `parameter` can contain extra information, here it can be `on` to turn the led on and anything else in the 
+* parameter will turn the led off.
+* 
+* Here we're looking for a message in the format `server:rgb:#nnnnnn`, 
+* where the #nnnnnn part is a hexidecimal color code (see the readme)
+* If we recieve this message and it is formatted properly, color all 
+* the pixels of the ledstrip to this color.
+*/
+
+void messageReceived(String &topic, String &payload)
+{
+	Serial.println(topic + ": " + payload);
+
+	StringSplitter *splitter = new StringSplitter(payload, ':', 3);
+	int itemCount = splitter->getItemCount();
+
+	if (itemCount == 3)
+	{ // we have properly formatted command
+		String origin = splitter->getItemAtIndex(0);
+		String command = splitter->getItemAtIndex(1);
+		String parameter = splitter->getItemAtIndex(2);
+
+		if (origin == "server")
+		{
+			if (command == "rgb")
+			{
+				Serial.println(parameter.length());
+				if (parameter.length() == 7 && parameter.startsWith("#"))
+				{
+					// get the R, G and B parts of the string
+					String r = parameter.substring(1, 3);
+					String g = parameter.substring(3, 5);
+					String b = parameter.substring(5, 7);
+					Serial.print(r);
+					Serial.print(" ");
+					Serial.print(g);
+					Serial.print(" ");
+					Serial.println(b);
+
+					// convert the hex values to decimal
+					int rr = StrToHex(r.c_str());
+					int gg = StrToHex(g.c_str());
+					int bb = StrToHex(b.c_str());
+
+					// then update the pixel colors
+					for (int i = 0; i < NUMPIXELS; i++) { 
+						// pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+						pixels.setPixelColor(i, pixels.Color(rr, gg, bb));
+						pixels.show(); // Send the updated pixel colors to the hardware.
+						delay(100);
+					}
+				}
+			}
+		}
+	}
+}
+
+/*
+* This function converts a hexademical number (base 16) into
+* a decimal number. 
+*/
+int StrToHex(const char str[])
+{
+	return (int)strtol(str, 0, 16);
+}
+
+/* ---------------------- you can ignore code below this line --------------------- */
+
+/*
+ * Wait for wifi connection
+ */
+void connectWifi()
+{
+	Serial.print("Connecting to wifi...");
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print(".");
+		delay(1000);
+	}
+	Serial.println("\nWifi connected!");
+	delay(1000);
+}
+
+/*
+ * Wait for MQTT connection and subscribe to the topic
+ */
+void connectMqtt()
+{
+	Serial.print("\nConnecting to MQTT server...");
+	while (!client.connect(client_id.c_str()))
+	{
+		Serial.print(".");
+		delay(1000);
+	}
+	Serial.println("\nMQTT connected!");
+	delay(1000);
+	client.subscribe(topic);
+}
+
+/*
+ * Concat the command and parameter into a colon separated string
+ * and publish the message
+ */
+void sendMessage(String message, String parameter)
+{
+	String msg = client_id + ":" + message + ":" + parameter;
+	client.publish(topic, msg);
+}
+
+/*
+ * Overloaded sendMessage function without the parameter
+ */
+void sendMessage(String message)
+{
+	sendMessage(message, "");
+}
+
+```
